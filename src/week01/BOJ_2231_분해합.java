a4fd26db7fab9a82c1abdf3f922b224aa01fc80a
hyeondo <eun041212@gmail.com>
2025-01-06 18:06:48 +0900

    feat(BOJ_2231_분해합) : 1주 차 문제 1 구현

---
+++ src/week01/BOJ_2231_분해합.java
@@ -0,0 +1,39 @@
+package week01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2231_분해합 {
+
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        if (find()) {
+            return;
+        }
+
+        System.out.println(0);
+    }
+
+    private static boolean find() {
+        for (int i = 1; i <= N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (i + sum == N) {
+                System.out.println(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/week01/Main.java
+++
@@ -1,8 +0,0 @@
-package week01;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // TODO : 정답 코드 작성
-    }
-}
