49e9f87f853e6ddb9db78c789adb293411272eef
hyeondo <eun041212@gmail.com>
2025-01-12 12:57:03 +0900

    refactor(BOJ_2231_분해합)

--- src/week1/BOJ_2231_분해합.java
+++ src/week1/BOJ_2231_분해합.java
@@ -11,18 +11,30 @@ public class BOJ_2231_분해합 {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            // 1. N 입력받기
             int N = Integer.parseInt(br.readLine());
-            for (int number = 1; number <= N; number++) {
-                int sum = 0;
+            // 1~N까지 숫자들을 생성자로써 순회
+            for (int i = 1; i <= N; i++) {
+                int number = i;
+                int digitSum = 0;
+                // 각 숫자의 분해합을 구함
+                // 숫자가 양의 정수일 때
                 while (number > 0) {
-                    sum += number % 10;
+                    // 분해합에
+                    // 숫자를 10으로 나눈 나머지인 마지막 자릿수를 더하고
+                    digitSum += number % 10;
+                    // 숫자를 10으로 나누어 다음 반복 때 다음 자리 수를 구할 수 있게 한다.
                     number /= 10;
                 }
-                if (number + sum == N) {
-                    bw.write(String.valueOf(number));
+                // 생성자와 분해합의 합이 N과 같다면
+                if (i + digitSum == N) {
+                    // 결과를 출력하고 프로그램을 종료
+                    bw.write(String.valueOf(i));
                     return;
                 }
             }
+            // N까지의 모든 숫자를 탐색했는데도 생성자를 찾지 못했다면
+            // 0을 출력
             bw.write(String.valueOf(0));
         } catch (IOException e) {
             e.printStackTrace();
