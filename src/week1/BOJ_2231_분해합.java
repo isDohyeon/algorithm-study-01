3aaa58327b87a89a8a72ac41b98f69e0d9b60c2d
hyeondo <eun041212@gmail.com>
2025-01-12 12:45:37 +0900

    refactor(BOJ_2231_분해합)

--- src/week1/BOJ_2231_분해합.java
+++ src/week1/BOJ_2231_분해합.java
@@ -1,39 +1,31 @@
 package week1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class BOJ_2231_분해합 {
 
-    private static int N;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-
-        if (find()) {
-            return;
-        }
-
-        System.out.println(0);
-    }
-
-    private static boolean find() {
-        for (int i = 1; i <= N; i++) {
-            int num = i;
-            int sum = 0;
-
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int N = Integer.parseInt(br.readLine());
+            for (int number = 1; number <= N; number++) {
+                int sum = 0;
+                while (number > 0) {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                if (number + sum == N) {
+                    bw.write(String.valueOf(number));
+                    return;
+                }
             }
-
-            if (i + sum == N) {
-                System.out.println(i);
-                return true;
-            }
+            bw.write(String.valueOf(0));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return false;
     }
 }
