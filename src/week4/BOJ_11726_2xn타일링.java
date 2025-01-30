71289ccb81349df758d53f06e1e101f0c0e2e3fb
hyeondo <eun041212@gmail.com>
2025-01-30 17:38:16 +0900

    feat(BOJ_11726_2xn타일링) : 4-4 문제해결

---
+++ src/week4/BOJ_11726_2xn타일링.java
@@ -0,0 +1,38 @@
+package week4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_11726_2xn타일링 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int result = getResult(N);
+
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+
+    private static int getResult(int N) {
+        if (N == 1) {
+            return 1;
+        }
+        if (N == 2) {
+            return 2;
+        }
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10_007;
+        }
+        return dp[N];
+    }
+}
